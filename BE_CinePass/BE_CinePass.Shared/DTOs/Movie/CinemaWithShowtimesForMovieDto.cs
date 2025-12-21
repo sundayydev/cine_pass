889c69be840f3313f1113725ddd5e81d6ce24cc4
Lê Duy Hoàng <129831728+Elihoang@users.noreply.github.com>
2025-12-21 18:11:03 +0700

    get brandcinema

--- BE_CinePass/BE_CinePass.Shared/DTOs/Movie/CinemaWithShowtimesForMovieDto.cs
+++ BE_CinePass/BE_CinePass.Shared/DTOs/Movie/CinemaWithShowtimesForMovieDto.cs
@@ -8,6 +8,9 @@ public class CinemaWithShowtimesForMovieDto
     public string CinemaName { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public string? Address { get; set; }
+    public string? City { get; set; }
+    public double? Latitude { get; set; }
+    public double? Longitude { get; set; }
     public string? Phone { get; set; }
     public string? BannerUrl { get; set; }
     public int TotalScreens { get; set; }
